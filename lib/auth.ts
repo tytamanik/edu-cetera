65c594d94c649fb6aff7c94d0f59f1d0399bad3f
tytamanik <watislavve@gmail.com>
2025-03-25 00:04:26 +0200

    Trigger redeploy

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,43 +1,43 @@
-import { isEnrolledInCourse } from "@/sanity/lib/student/isEnrolledInCourse";
-import { getStudentByClerkId } from "@/sanity/lib/student/getStudentByClerkId";
-import getCourseById from "@/sanity/lib/courses/getCourseById";
-
+import getCourseById from '@/sanity/lib/courses/getCourseById'
+import { getStudentByClerkId } from '@/sanity/lib/student/getStudentByClerkId'
+import { isEnrolledInCourse } from '@/sanity/lib/student/isEnrolledInCourse'
+//trigger redeploy
 interface AuthResult {
-  isAuthorized: boolean;
-  redirect?: string;
-  studentId?: string;
+	isAuthorized: boolean
+	redirect?: string
+	studentId?: string
 }
 
 export async function checkCourseAccess(
-  clerkId: string | null,
-  courseId: string
+	clerkId: string | null,
+	courseId: string
 ): Promise<AuthResult> {
-  if (!clerkId) {
-    return {
-      isAuthorized: false,
-      redirect: "/",
-    };
-  }
+	if (!clerkId) {
+		return {
+			isAuthorized: false,
+			redirect: '/',
+		}
+	}
 
-  const student = await getStudentByClerkId(clerkId);
-  if (!student?.data?._id) {
-    return {
-      isAuthorized: false,
-      redirect: "/",
-    };
-  }
+	const student = await getStudentByClerkId(clerkId)
+	if (!student?.data?._id) {
+		return {
+			isAuthorized: false,
+			redirect: '/',
+		}
+	}
 
-  const isEnrolled = await isEnrolledInCourse(clerkId, courseId);
-  const course = await getCourseById(courseId);
-  if (!isEnrolled) {
-    return {
-      isAuthorized: false,
-      redirect: `/courses/${course?.slug?.current}`,
-    };
-  }
+	const isEnrolled = await isEnrolledInCourse(clerkId, courseId)
+	const course = await getCourseById(courseId)
+	if (!isEnrolled) {
+		return {
+			isAuthorized: false,
+			redirect: `/courses/${course?.slug?.current}`,
+		}
+	}
 
-  return {
-    isAuthorized: true,
-    studentId: student.data._id,
-  };
+	return {
+		isAuthorized: true,
+		studentId: student.data._id,
+	}
 }
